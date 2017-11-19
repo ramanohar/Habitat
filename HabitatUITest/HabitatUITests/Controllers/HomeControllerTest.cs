d16f779e1fe164efaa2b363946ee0643f2a85aab
ramanohar <manohar.ra@gmail.com>
2017-11-19 10:08:41 -0800

    Add files via upload

---
+++ HabitatUITest/HabitatUITests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Drawing.Imaging;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using Applitools;
+using Applitools.Selenium;
+
+
+namespace HabitatUITests.Controllers
+{
+    [TestClass]
+    public class HomePageUITests
+    {
+        public Eyes eyes = new Eyes();
+        //System.setProperty("webdriver.firefox.marionette","G:\\Selenium\\Firefox driver\\geckodriver.exe");
+        private IWebDriver webDriver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            if (ConfigurationHelper.BrowserType == (int)BrowserType.Firefox)
+            {
+                webDriver = new FirefoxDriver();
+            }
+            else
+            {
+                webDriver = new ChromeDriver(ConfigurationHelper.ChromeDrive);
+            }
+
+            
+            eyes.ApiKey = "l64hbuXc2lxij1vHN0emmKavNATg9Vi7OEuepVC106x48110";
+        }
+        //    bool exists = System.IO.Directory.Exists(ConfigurationHelper.FolderPicture);
+
+        //    if (!exists)
+        //    {
+        //        System.IO.Directory.CreateDirectory(ConfigurationHelper.FolderPicture);
+        //    }
+        //}
+
+        [TestMethod]
+        public void UC001_LoadHomePage()
+        {
+            webDriver.Navigate().GoToUrl(ConfigurationHelper.SiteUrl);
+            
+            bool result = true;
+            try
+            {
+                //eyes.Open(webDriver, "Habitat", "UC001_LoadHomePage");
+                //eyes.CheckWindow("Test1");
+
+                result = (webDriver.Title.ToString().Contains("Google"));
+
+                //eyes.Close();
+            }
+            catch(Exception ex)
+            {
+                result = false;
+            }
+           finally
+            {
+                Assert.AreEqual(result, true);
+                CloseBrowser();
+            }
+
+        }
+
+
+        [TestMethod]
+        public void UC002_HomePageVerifyCarouselCount()
+        {
+            webDriver.Navigate().GoToUrl(ConfigurationHelper.SiteUrl);
+           
+            bool result = true;
+            try
+            {
+                //eyes.Open(webDriver, "Habitat", "UC002_HomePageVerifyCarouselCount");
+               // eyes.CheckWindow("Test2");
+                result = (webDriver.FindElement(By.ClassName("carousel-indicators")).FindElements(By.TagName("li")).Count == 4);
+                //eyes.Close();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            finally
+            {
+                Assert.AreEqual(result, true);
+                CloseBrowser();
+            }
+           
+        }
+
+        [TestMethod]
+        public void UC003_LoadForgotPasswordPage()
+        {
+            webDriver.Navigate().GoToUrl(ConfigurationHelper.ForgotPasswordUrl);
+           
+            bool result = true;
+            try
+            {
+                //eyes.Open(webDriver, "Habitat", "UC003_LoadForgotPasswordPage");
+               // eyes.CheckWindow("Test3");
+                result = (webDriver.Title.ToString().Contains("Forgot Password"));
+               // eyes.Close();
+            }
+            catch(Exception ex)
+            {
+                result = false;
+            }
+           finally
+            {
+                Assert.AreEqual(result, true);
+                CloseBrowser();
+            }
+        }
+
+
+        //private void SaveScreenshot(Screenshot screenshot, string fileName)
+        //{
+        //    screenshot.SaveAsFile(string.Format("{0}{1}", ConfigurationHelper.FolderPicture, fileName), ImageFormat.Png);
+        //}
+
+        private void CloseBrowser()
+        {
+            webDriver.Quit();
+            //eyes.AbortIfNotClosed();
+            Thread.Sleep(1000);
+        }
+    }
+}
